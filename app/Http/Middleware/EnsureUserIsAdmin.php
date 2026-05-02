da01ff67c052050be2a2b929b816dfee8a18cdd8
mawinigop <mawin.mandocdoc@gmail.com>
2026-05-02 12:02:57 +0800

    updating the EnsureUserIsAdmin for naming convention

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Iche-check kung WALANG naka-login O KUNG HINDI admin
+        if (! $request->user() || ! $request->user()->isAdmin()) {
+            abort(403, 'Access denied. Admins only.');
+        }
+
+        return $next($request); // Pababayaan siyang tumuloy kung admin siya
+    }
+}
