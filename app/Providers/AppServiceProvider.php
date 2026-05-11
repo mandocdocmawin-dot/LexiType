0133fe23bd07cae82ba975036e5330e9d9216541
mawinigop <mawin.mandocdoc@gmail.com>
2026-05-12 01:28:45 +0800

    added pf forcescheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
 
         Gate::policy(UserFeedback::class, UserFeedbackPolicy::class);
